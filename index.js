02b96c1fbd23c96fb2b4a08f74174c34a7835135
rcfrak <frakerr@oregonstate.edu>
2024-11-14 13:34:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 2133;
+const name_array = [];
+
+app.use(cors());
+
+function generateUsername() {
+    const first = ['Silly', 'Handsome', 'Wild', 'Noble', 'Beautiful', 'Charming', 'Shy', 'Mysterious'];
+    const second = ['Guy', 'Girl', 'Dog', 'Cat', 'Dragon', 'Cowboy', 'Cowgirl', 'Astronaut'];
+    const first_word = first[Math.floor(Math.random() * first.length)];
+    const second_word = second[Math.floor(Math.random() * second.length)];
+    const year = Math.floor(Math.random() * (2006 - 1900) + 1900);
+    const name = `${first_word}_${second_word}_${year}`;
+    const name_match = name_array.includes(name)
+
+    if (name_match) {
+        return generateUsername();
+    } else {
+        name_array.push(name);
+        return name;
+    }
+};
+
+app.get('/api/username', (req, res) => {
+    const username = generateUsername();
+    res.json({ username });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running...`);
+});
